89801d9cdaff146e81dda39fd0ee2a7e1805b203
willodrome <65831821+willodrome@users.noreply.github.com>
2021-01-21 02:35:00 +0200

    dfsdf wq

--- Front_end/src/Pages/PatientHome.js
+++ Front_end/src/Pages/PatientHome.js
@@ -1,9 +1,32 @@
 import { ListAppointments } from "../Components/ListAppointments";
 import { Container } from "react-bootstrap";
 import { DrNav } from "../Components/DrNav";
+import axios from "axios";
 
 export const PatientHome = () => {
   // request to api based on patient_id
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    const result = axios.post("http://localhost:5000/api/register-patient", {
+      headers: {
+        // "auth-token": localStorage.getItem("UserToken"),
+        data: {
+          fullName: "Mr. Test User",
+          phone: "999000000",
+          email: "test@test.test",
+          birthDay: "31-12-2021",
+          gender: "m",
+          location: "8582 Leeton Ridge Lane Henderson, KY 42420",
+          maritalStatus: "married",
+          password: "test@test.test",
+        },
+      },
+    });
+
+    console.log(result);
+  };
+
   const mockAppointments = [
     {
       datetime_schedule: Date.now(),
@@ -36,6 +59,9 @@ export const PatientHome = () => {
       <DrNav />
       <h1>Patient Home</h1>
       <ListAppointments items={mockAppointments} />
+      <form onSubmit={handleSubmit}>
+        <button type="submit">Register Fake User</button>
+      </form>
     </Container>
   );
 };
